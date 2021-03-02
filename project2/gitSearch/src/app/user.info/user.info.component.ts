5a46d55bceb41420655ec5109bd455bb3fe38dc1
=FrancineU <uwizeyimanafrancine62@gmail.com>
2021-03-02 13:38:19 +0200

    added search function

--- project2/gitSearch/src/app/user.info/user.info.component.ts
+++ project2/gitSearch/src/app/user.info/user.info.component.ts
@@ -1,15 +1,38 @@
+
 import { Component, OnInit } from '@angular/core';
+import {ServiceService} from '../services/service.service';
+import {Repository} from '../repository';
+import {User} from '../user';
 
 @Component({
-  selector: 'app-user.info',
-  templateUrl: './user.info.component.html',
-  styleUrls: ['./user.info.component.css']
+  selector: 'app-user-info',
+  templateUrl: './user-info.component.html',
+  providers: [ServiceService],
+  styleUrls: ['./user-info.component.css']
 })
-export class User.InfoComponent implements OnInit {
+export class UserInfoComponent implements OnInit {
 
-  constructor() { }
+  public searchMe = 'FrancineU';
+  public githubUser: string;
 
-  ngOnInit(): void {
+
+  users: User ;
+  repository: Repository;
+
+
+  findUser(username) {
+      this.githubUser = '';
+      this.searchMe  = username;
+      this.ngOnInit();
   }
 
+
+constructor(public githubUserRequest: ServiceService, public userRepos: ServiceService) { }
+
+ngOnInit() {
+    this.githubUserRequest.githubUser(this.searchMe);
+    this.users = this.githubUserRequest.users;
+    this.userRepos.gitUserRepos(this.searchMe);
+    console.log(this.userRepos);
+}
 }
